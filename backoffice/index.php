ccf48cc39e275fc604db1101018cea0626030622
c42759 <carlos@nexus-pt.eu>
2015-03-06 22:43:40 +0000

    config folder

--- backoffice/index.php
+++ backoffice/index.php
@@ -1,31 +1,31 @@
 <?php
 
-include "backoffice/class/PHPMailer/class.phpmailer.php";
-include "backoffice/configuration.php";
-include "backoffice/connect.php";
-include "backoffice/functions.php";
+include "class/PHPMailer/class.phpmailer.php";
+include "configuration.php";
+include "connect.php";
+include "functions.php";
 include "pages/functions.php";
 
 // controlador de sessão
 if (isset($_COOKIE[$configuration["cookie"]]) && !empty($_COOKIE[$configuration["cookie"]])) {
 
-    $cookie = explode(".", $_COOKIE[$configuration["cookie"]]);
+	$cookie = explode(".", $_COOKIE[$configuration["cookie"]]);
 
-    if (count($cookie) === 2) {
-        $cookie[0] = intval($cookie[0]);
-        $cookie[1] = $mysqli->real_escape_string($cookie[1]);
+	if (count($cookie) === 2) {
+		$cookie[0] = intval($cookie[0]);
+		$cookie[1] = $mysqli->real_escape_string($cookie[1]);
 
-        $query[0] = sprintf("SELECT * FROM %s_users WHERE id = '%s' AND password = '%s' LIMIT %s", $configuration["mysql-prefix"], $cookie[0], $cookie[1], 1);
-        $source[0] = $mysqli->query($query[0]);
-        $nr[0] = $source[0]->num_rows;
+		$query[0] = sprintf("SELECT * FROM %s_users WHERE id = '%s' AND password = '%s' LIMIT %s", $configuration["mysql-prefix"], $cookie[0], $cookie[1], 1);
+		$source[0] = $mysqli->query($query[0]);
+		$nr[0] = $source[0]->num_rows;
 
-        if ($nr[0] === 1) {
-            $auth = true;
-            $authData = $source[0]->fetch_assoc();
-        } else {
-            $auth = false;
-        }
-    }
+		if ($nr[0] === 1) {
+			$auth = true;
+			$authData = $source[0]->fetch_assoc();
+		} else {
+			$auth = false;
+		}
+	}
 } else {
 	$auth = false;
 }
@@ -62,14 +62,14 @@ if (isset($_GET["i"]) && !empty($_GET["i"])) {
 
 // controlador de acção
 if (isset($_GET["a"]) && !empty($_GET["a"])) {
-    $a = $mysqli->real_escape_string($_GET["a"]);
+	$a = $mysqli->real_escape_string($_GET["a"]);
 } else {
-    $a = null;
+	$a = null;
 }
 
 $head = file_get_contents("templates-e/head.html");
 
-$language = parse_ini_file(sprintf("./languages/%s.ini", $lg_s), true);
+$language = parse_ini_file(sprintf("languages/%s.ini", $lg_s), true);
 
 /*
  *  abaixo é iniciada a criação do template, com base nós ficheiros html
