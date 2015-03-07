24641d905c1a27986115432c3217b3281b2b751c
c42759 <carlos@nexus-pt.eu>
2015-03-07 01:56:06 +0000

    set versions

--- backoffice/config/system.php
+++ backoffice/config/system.php
@@ -12,8 +12,8 @@ $cfg->system->owner = "";
 $cfg->system->path = "Path-here ex.: /new or /old";
 $cfg->system->path_bo = "Path-above + bo folder ex.: /new/backoffice";
 
-$cfg->system->version = "";
-$cfg->system->sub_version = "";
+$cfg->system->version = "3.0.0";
+$cfg->system->sub_version = "alpha";
 
 $cfg->system->key = "GJTBpKregE9WgXc";
 
