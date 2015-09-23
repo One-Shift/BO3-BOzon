2917d972ed4e45f785811a7f4b339bca761d2c3d
Carlos Santos <carlos@nexus-pt.eu>
2015-09-23 15:11:35 +0100

    verify new propertu

--- backoffice/config/connect.php
+++ backoffice/config/connect.php
@@ -1,15 +1,17 @@
 <?php
+if ($cfg->db->connect) {
+	$mysqli = mysqli_connect(
+		$cfg->db->host,
+		$cfg->db->user,
+		$cfg->db->password,
+		$cfg->db->database
+	);
 
-$mysqli = mysqli_connect(
-	$cfg->db->host,
-	$cfg->db->user,
-	$cfg->db->password,
-	$cfg->db->database
-);
 
-if (mysqli_connect_errno()) {
-	printf("Connect failed: %s\n", mysqli_connect_error());
-	exit();
+	if (mysqli_connect_errno()) {
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+	}
+
+	$mysqli->set_charset("utf8");
 }
-
-$mysqli->set_charset("utf8");
