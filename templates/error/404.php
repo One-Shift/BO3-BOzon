e64802a10d4bd42eab98ce31cf09b8ab52a5fbca
Carlos Santos <carlos@nexus-pt.eu>
2015-09-23 16:08:21 +0100

    add 404 template

--- templates/error/404.php
+++ templates/error/404.php
@@ -1,1 +1,23 @@
 <!DOCTYPE html>
+<html lang="{c2r-lg}">
+	<head>
+		{c2r-head}
+	</head>
+	<body>
+		<div class="home">
+			<!-- here write the HTML code -->
+			<div class="container">
+				<div class="text-center">
+					<h1 class="big">Oops,</h1>
+					<h2>404!</h2>
+					<!--h3>A página não existe.</h3-->
+					<div class="text">
+						<p>Parece que a página buscada não existe ou algum erro ocorreu.</p>
+						<p>Clique <a href="{c2r-path}/0/{c2r-lg}/">aqui</a> para voltar</p>
+					</div>
+				</div>
+			</div>
+			<!-- Marianna Almeida - contato@agyrafa.com -->
+		</div>
+	</body>
+</html>
