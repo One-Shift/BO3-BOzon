e5e787d556e253927c86d05d4934d2e02049ba9b
Carlos Santos <carlos@nexus-pt.eu>
2015-09-24 10:24:14 +0100

    based file

---
+++ backoffice/modules/sys-404/sys-404.php
@@ -0,0 +1,7 @@
+<?php
+
+$cfg->mod = new stdClass();
+$cfg->mod->name = "404 Page Not Found";
+$cfg->mod->version = "0.0.1";
+$cfg->mod->developer = "developer name here";
+$cfg->mod->contact = "developer@email.here";
