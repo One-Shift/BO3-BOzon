94e8f323098f08f8dff452c3f506466a525bb773
Carlos Santos <carlos@nexus-pt.eu>
2015-09-25 16:26:50 +0100

    add base system

--- backoffice/modules/sys-login/actions/home.php
+++ backoffice/modules/sys-login/actions/home.php
@@ -1,1 +1,42 @@
 <?php
+
+$page_tpl = file_get_contents(sprintf("modules/%s/templates/home.html", $cfg->mod->folder));
+
+$form = str_replace(
+	[],
+	[],
+	file_get_contents(sprintf("modules/%s/templates-e/form.html", $cfg->mod->folder))
+);
+
+if (isset($_POST["submit"])) {
+	$form = str_replace(
+		[
+			"{c2r-return-message}"
+		],
+		[
+			str_replace(
+				[
+					"{c2r-message}"
+				],
+				[
+					"Message HERE"
+				],
+				file_get_contents(sprintf("modules/%s/templates-e/return-message.html", $cfg->mod->folder))
+			)
+		],
+		$form
+	);
+} else {
+	$form = str_replace("{c2r-return-message}", null, $form);
+}
+
+/* last thing */
+$tpl = str_replace(
+	[
+		"{c2r-form}"
+	],
+	[
+		$form
+	],
+	$page_tpl
+);
