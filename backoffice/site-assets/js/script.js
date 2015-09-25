0af04a03c799268a93cf47049aa7e75c68d1d9c4
Carlos Santos <carlos@nexus-pt.eu>
2015-09-25 16:27:08 +0100

    add some scripts

--- backoffice/site-assets/js/script.js
+++ backoffice/site-assets/js/script.js
@@ -1,1 +1,29 @@
 // scripting here
+function hideMenu (o) {
+    o.animate( {'left':'-300px'}, {queue: false, duration: 500});
+}
+
+function showMenu (o) {
+    o.animate( {"left": "0"}, {queue: false, duration: 500});
+}
+
+$(document).ready(function () {
+    $("#bo-menu-button").on("click", function () {
+        var container = $("#bo-menu");
+        showMenu(container);
+    });
+
+    // get avatar for login page
+    $("body.login input[name=name]").on("focusout",
+        function () {
+            $("#avatar").attr("src", 'http://www.gravatar.com/avatar/' + md5($("input[name=name]").val()) + "?s=240&r=g&d=mm");
+        }
+    )
+});
+
+$(document).mouseup(function (e) {
+    var container = $("#bo-menu");
+    if (!container.is(e.target) /* if the target of the click isn't the container... */ && container.has(e.target).length === 0) {
+        hideMenu(container);
+    }
+});
