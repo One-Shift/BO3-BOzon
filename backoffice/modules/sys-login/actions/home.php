b030903880efeab4123a917223cd527dec3c0d63
Carlos Santos <carlos@nexus-pt.eu>
2016-05-17 14:47:28 +0100

    new v

--- backoffice/modules/sys-login/actions/home.php
+++ backoffice/modules/sys-login/actions/home.php
@@ -7,42 +7,84 @@ $form = str_replace(
 	[],
 	file_get_contents(sprintf("modules/%s/templates-e/form.html", $cfg->mod->folder))
 );
-if (isset($_POST["submit"])) { print_r($_POST);
-	if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) && !empty($_POST["password"])) { print 1;
-		$user = new user(); $_POST["password"] = $user->getSecurePassword($_POST["password"]);
+if (isset($_POST["submit"])) { var_dump($_POST);
+	if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+		if (!empty($_POST["password"])) {
+			print 1;
 
-		$query = sprintf(
-			"SELECT id, password FROM %s_users WHERE email = '%s' AND password = '%s' AND status = '%s' AND rank != '%s' LIMIT %s",
-			$cfg->db->prefix, $_POST["email"], $_POST["password"], TRUE, "member", 1
-		);
+			$_POST["password"] = user::getSecurePassword($_POST["password"]);
 
-		$source = $mysqli->query($query);
+			$query = sprintf(
+				"SELECT id, password FROM %s_users WHERE email = '%s' AND password = '%s' AND status = '%s' AND rank != '%s' LIMIT %s",
+				$cfg->db->prefix, $_POST["email"], $_POST["password"], TRUE, "member", 1
+			);
 
-		if ($source->num_rows > 0) {
-			$data = $source->fetch_assoc();
+			$source = $mysqli->query($query);
 
-			if (setcookie($cfg->system->cookie, ($data["id"].".".$data["password"]), time() + ($cfg->system->cookie_time * 60))) {
-				header("Location: ".$cfg->system->path_bo."/0/$lg_s/example/");
-			} else {
-				// ERROR MESSAGE
-				$form = str_replace(
-					[
-						"{c2r-return-message}"
-					],
-					[
-						str_replace(
+			if ($source->num_rows > 0) {
+				$data = $source->fetch_assoc();
+
+				if (setcookie($cfg->system->cookie, ($data["id"].".".$data["password"]), time() + ($cfg->system->cookie_time * 60), "/", $_SERVER['REQUEST_URI'])) {
+					//header("Location: ".$cfg->system->path_bo."/0/$lg_s/example/");
+
+					// SUCCESS MESSAGE
+					$form = str_replace(
 						[
-							"{c2r-message}"
+							"{c2r-return-message}"
 						],
 						[
-							"Message HERE"
+							str_replace(
+							[
+								"{c2r-message}"
+							],
+							[
+								"COOL!"
+							],
+							file_get_contents(sprintf("modules/%s/templates-e/return-message.html", $cfg->mod->folder))
+							)
 						],
-						file_get_contents(sprintf("modules/%s/templates-e/return-message.html", $cfg->mod->folder))
-						)
-					],
-					$form
-				);
+						$form
+					);
+				} else {
+					// ERROR MESSAGE
+					$form = str_replace(
+						[
+							"{c2r-return-message}"
+						],
+						[
+							str_replace(
+							[
+								"{c2r-message}"
+							],
+							[
+								"Message HERE"
+							],
+							file_get_contents(sprintf("modules/%s/templates-e/return-message.html", $cfg->mod->folder))
+							)
+						],
+						$form
+					);
+				}
 			}
+		} else {
+			// ERROR MESSAGE
+			$form = str_replace(
+				[
+					"{c2r-return-message}"
+				],
+				[
+					str_replace(
+					[
+						"{c2r-message}"
+					],
+					[
+						"Message HERE"
+					],
+					file_get_contents(sprintf("modules/%s/templates-e/return-message.html", $cfg->mod->folder))
+					)
+				],
+				$form
+			);
 		}
 	} else {
 		// ERROR MESSAGE
