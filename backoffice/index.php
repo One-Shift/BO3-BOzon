d0d7d81b659428cefe205c36a545658fb26a7e1f
Carlos Santos <carlos@nexus-pt.eu>
2016-05-17 10:07:43 +0100

    correct file version

--- backoffice/index.php
+++ backoffice/index.php
@@ -68,32 +68,19 @@ $tpl = str_replace(
 				"{c2r-lib-fontawesome}"
 			],
 			[
-<<<<<<< HEAD
 				(isset($og["title"])) ? $og["title"] : $cfg->system->sitename,
 				(isset($og["url"])) ? $og["url"] : "http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"],
 				(isset($og["image"])) ? $og["image"] : "http://".$_SERVER["HTTP_HOST"].$cfg->system->path."/site-assets/default-share-image.jpg",
 				(isset($og["description"])) ? $og["description"] : $lang["system"]["description"],
-=======
-				(isset($og["title"])) ? $og["title"] : $configuration["site-name"],
-				(isset($og["url"])) ? $og["url"] : "http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"],
-				(isset($og["image"])) ? $og["image"] : "http://".$_SERVER["HTTP_HOST"].$configuration["path"]."/site-assets/default-share-image.jpg",
-				(isset($og["description"])) ? $og["description"] : $language["system"]["description"],
->>>>>>> origin/master
 				file_get_contents("http://nexus-pt.github.io/BO2/jquery.html"),
 				file_get_contents("http://nexus-pt.github.io/BO2/bootstrap.html"),
 				file_get_contents("http://nexus-pt.github.io/BO2/fontawesome.html")
 			],
 			$head
 		),
-<<<<<<< HEAD
 		$cfg->system->sitename,
 		$lang["system"]["keywords"],
 		$lang["system"]["description"],
-=======
-		$cfg->system->name,
-		$language["system"]["keywords"],
-		$language["system"]["description"],
->>>>>>> origin/master
 		$cfg->system->path_bo,
 		$lg_s
 	],
--- index.php
+++ index.php
@@ -1,49 +1,34 @@
 <?php
 
-include "class/PHPMailer/class.phpmailer.php";
-include "class/class.user.php";
-include "class/class.functions.php";
+include "backoffice/class/PHPMailer/class.phpmailer.php";
+include "backoffice/class/class.functions.php";
 
-include "config/cfg.php";
-include "config/database.php";
-include "config/email.php";
-include "config/languages.php";
-include "config/store.php";
-include "config/system.php";
-include "config/connect.php";
+include "backoffice/config/cfg.php";
+include "backoffice/config/database.php";
+include "backoffice/config/email.php";
+include "backoffice/config/languages.php";
+include "backoffice/config/store.php";
+include "backoffice/config/system.php";
+include "backoffice/config/connect.php";
 
-include "controller/languages.php";
-include "controller/sessions.php";
-include "controller/pages.php";
-include "controller/actions.php";
-include "controller/id.php";
+include "backoffice/controller/languages.php";
+include "backoffice/controller/sessions.php";
+include "backoffice/controller/pages.php";
+include "backoffice/controller/actions.php";
+include "backoffice/controller/id.php";
 
 $head = file_get_contents("templates-e/head.html");
 
-// abaixo é iniciada a criação do template, com base nós ficheiros html
-
-if ($auth) {
-	switch ($pg) {
-		case "logout":
-			include sprintf("modules/sys-%s/sys-%s.php", "logout","logout");
-			break;
-		case "login":
-			include sprintf("modules/sys-%s/sys-%s.php", "login","login");
-			break;
-		default:
-			$mod_path = sprintf("modules/mod-%s/", $pg);
-
-			if (!file_exists($mod_path)) {
-				include sprintf("modules/sys-%s/sys-%s.php", "404","404");
-				exit;
-			} else {
-				// mod load
-				include sprintf("%smod-%s.php", $mod_path,$pg);
-			}
-			break;
-	}
+// page controller
+$pg_file = sprintf("pages/%s.php", $pg);
+if ($pg == null) {
+	include "pages/home.php";
+} else if ($pg == "404") {
+	include "pages/404.php";
+} else if (file_exists($pg_file)) {
+	include $pg_file;
 } else {
-	include sprintf("modules/sys-%s/sys-%s.php", "login","login");
+	include "pages/404.php";
 }
 
 // print website
@@ -53,6 +38,7 @@ $tpl = str_replace(
 		"{c2r-sitename}",
 		"{c2r-keywords}",
 		"{c2r-description}",
+		"{c2r-analytics}",
 		"{c2r-path}",
 		"{c2r-lg}"
 	],
@@ -68,20 +54,21 @@ $tpl = str_replace(
 				"{c2r-lib-fontawesome}"
 			],
 			[
-				(isset($og["title"])) ? $og["title"] : $cfg->system->sitename,
+				(isset($og["title"])) ? $og["title"] : $configuration["site-name"],
 				(isset($og["url"])) ? $og["url"] : "http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"],
-				(isset($og["image"])) ? $og["image"] : "http://".$_SERVER["HTTP_HOST"].$cfg->system->path."/site-assets/default-share-image.jpg",
-				(isset($og["description"])) ? $og["description"] : $lang["system"]["description"],
+				(isset($og["image"])) ? $og["image"] : "http://".$_SERVER["HTTP_HOST"].$configuration["path"]."/site-assets/default-share-image.jpg",
+				(isset($og["description"])) ? $og["description"] : $language["system"]["description"],
 				file_get_contents("http://nexus-pt.github.io/BO2/jquery.html"),
 				file_get_contents("http://nexus-pt.github.io/BO2/bootstrap.html"),
 				file_get_contents("http://nexus-pt.github.io/BO2/fontawesome.html")
 			],
 			$head
 		),
-		$cfg->system->sitename,
-		$lang["system"]["keywords"],
-		$lang["system"]["description"],
-		$cfg->system->path_bo,
+		$cfg->system->name,
+		$language["system"]["keywords"],
+		$language["system"]["description"],
+		$cfg->system->analytics,
+		$cfg->system->path,
 		$lg_s
 	],
 	$tpl
