ecdc8a42f41662d82cdc1565bd7951fa9574c336
Carlos Santos <c42759@users.noreply.github.com>
2016-09-30 11:30:00 +0100

    Update email.php

--- backoffice/config/email.php
+++ backoffice/config/email.php
@@ -8,3 +8,5 @@ $cfg->email->username = "your@email.here";
 $cfg->email->password = "yourpassword";
 $cfg->email->secure = "connection-type eg.: plain";
 $cfg->email->port = "connection-port eg.: 25";
+
+$cfg->email->support = "geral@nexus-pt.eu";
