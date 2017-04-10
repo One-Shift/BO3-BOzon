1c43bc64ad321d77723d66cb53a0f5cc8c75fcd9
Carlos Santos <carlos@one-shift.com>
2017-04-10 16:55:17 +0100

    Update mod-home

--- backoffice/modules/mod-home/actions/home.php
+++ backoffice/modules/mod-home/actions/home.php
@@ -1,15 +1,15 @@
 <?php
 
 $mdl = str_replace(
-	[
-		"{c2r-version}",
-		"{c2r-sub-version}"
-	],
-	[
-		$cfg->system->version,
-		$cfg->system->sub_version
-	],
-	functions::mdl_load("templates/home.tpl")
+    [
+        "{c2r-version}",
+        "{c2r-sub-version}"
+    ],
+    [
+        $cfg->system->version,
+        $cfg->system->sub_version
+    ],
+    functions::mdl_load("templates/home.tpl")
 );
 
 include "pages/module-core.php";
