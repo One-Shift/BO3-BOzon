786a5922290b569afd110b29221f712ac45df71f
Carlos Santos <c42759@users.noreply.github.com>
2017-05-09 17:30:57 +0100

    Update email.php

--- backoffice/config/email.php
+++ backoffice/config/email.php
@@ -2,11 +2,11 @@
 
 $cfg->email = new stdClass();
 
-$cfg->email->email = ["geral@nexus-pt.eu"];
+$cfg->email->email = ["hello@one-shift.com";"];
 $cfg->email->smtp = "mail.yourdomain.here";
 $cfg->email->username = "your@email.here";
 $cfg->email->password = "yourpassword";
 $cfg->email->secure = "connection-type eg.: plain";
 $cfg->email->port = "connection-port eg.: 25";
 
-$cfg->email->support = "geral@nexus-pt.eu";
+$cfg->email->support = "hello@one-shift.com";
