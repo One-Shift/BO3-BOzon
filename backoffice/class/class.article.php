0876c16ef00fa0a182359eaaf5c3126a8e526b23
Carlos Santos <c42759@users.noreply.github.com>
2017-06-13 19:30:56 +0100

    bugfix - save on multiple languages error, solved

--- backoffice/class/class.article.php
+++ backoffice/class/class.article.php
@@ -104,17 +104,17 @@ class article {
 			$toReturn = true;
 
 			foreach ($cfg->lg as $index => $lg) {
-				$fast_query = sprintf(
-					"SELECT id FROM %s_articles_lang WHERE article_id = %s AND lang_id = %s",
-					$cfg->db->prefix,
-					$this->id,
-					$index
-				);
+				if ($lg[0]) {
+					$fast_query = sprintf(
+						"SELECT id FROM %s_articles_lang WHERE article_id = %s AND lang_id = %s",
+						$cfg->db->prefix,
+						$this->id,
+						$index
+					);
 
-				$fast_source = $mysqli->query($fast_query);
+					$fast_source = $mysqli->query($fast_query);
 
-				if ($fast_source->num_rows > 0) {
-					if($lg[0]){
+					if ($fast_source->num_rows > 0) {
 						$query[$index] = sprintf("UPDATE %s_articles_lang SET title = '%s', text = '%s' WHERE article_id = '%s' AND lang_id = '%s'",
 							$cfg->db->prefix,
 							$this->title[$index-1],
@@ -124,17 +124,17 @@ class article {
 						);
 
 						$mysqli->query($query[$index]);
-					}
-				} else {
-					$query[1] = sprintf("INSERT INTO %s_articles_lang (article_id, lang_id, title, text) VALUES (%s, %s, '%s', '%s')",
-						$cfg->db->prefix,
-						$this->id,
-						$index,
-						$this->title[$index - 1],
-						$this->description[$index - 1]
-					);
+					} else {
+						$query[1] = sprintf("INSERT INTO %s_articles_lang (article_id, lang_id, title, text) VALUES (%s, %s, '%s', '%s')",
+							$cfg->db->prefix,
+							$this->id,
+							$index,
+							$this->title[$index - 1],
+							$this->description[$index - 1]
+						);
 
-					$mysqli->query($query[1]);
+						$mysqli->query($query[1]);
+					}
 				}
 			}
 		}
