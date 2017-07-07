6bbadb387fcf72adb337f50c2c87eccc255b536b
Carlos Santos <carlos@one-shift.com>
2017-07-07 12:52:49 +0100

    minify some script code

--- site-assets/js/script.js
+++ site-assets/js/script.js
@@ -1,28 +1,7 @@
-// scripting here
-(
-	function ($) {
-		$.fn.serializeObject = function () { var o = {};
-		var a = this.serializeArray();
-		$.each(
-			a,
-			function () {
-				if (o[this.name]) {
-					if (!o[this.name].push) {
-						o[this.name] = [o[this.name]];
-					}
-
-					o[this.name].push(this.value || '');
-				} else {
-					o[this.name] = this.value || '';
-				}
-			});
-			return o;
-		};
-	}
-)(jQuery);
+( function ($) { $.fn.serializeObject = function () { var o = {}; var a = this.serializeArray(); $.each( a, function () { if (o[this.name]) { if (!o[this.name].push) { o[this.name] = [o[this.name]]; } o[this.name].push(this.value || ''); } else { o[this.name] = this.value || ''; } }); return o; }; } )(jQuery);
 
 $(document).ready(
 	function () {
-		
+
 	}
 );
