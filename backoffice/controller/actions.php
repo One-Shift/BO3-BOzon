e71126969c08bdd297d3956714d4623d048b4e13
Carlos Santos <c42759@users.noreply.github.com>
2017-09-13 12:01:00 +0100

    Update actions.php

--- backoffice/controller/actions.php
+++ backoffice/controller/actions.php
@@ -2,7 +2,7 @@
 
 // controlador de acção
 if (isset($_GET["a"]) && !empty($_GET["a"])) {
-	$a = $mysqli->real_escape_string($_GET["a"]);
+	$a = $db->real_escape_string($_GET["a"]);
 } else {
 	$a = null;
 }
