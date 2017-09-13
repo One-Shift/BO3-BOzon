6fdb4b1f784f6931f095bd6a3f00164d192a8dd2
Carlos Santos <c42759@users.noreply.github.com>
2017-09-13 11:29:22 +0100

    Update sessions-bo.php

--- backoffice/controller/sessions-bo.php
+++ backoffice/controller/sessions-bo.php
@@ -5,13 +5,13 @@ if ($cfg->db->connect) {
 		$cookie = explode(".", $_COOKIE[$cfg->system->cookie]);
 		if (count($cookie) === 2) {
 			$cookie[0] = intval($cookie[0]);
-			$cookie[1] = $mysqli->real_escape_string($cookie[1]);
+			$cookie[1] = $db->real_escape_string($cookie[1]);
 
 			$query = sprintf(
 				"SELECT * FROM %s_users WHERE id = '%s' AND password = '%s' AND (rank = 'owner' OR rank = 'manager') LIMIT %s",
 				$cfg->db->prefix, $cookie[0], $cookie[1], 1
 			);
-			$source = $mysqli->query($query);
+			$source = $db->query($query);
 
 			if ($source->num_rows == 1) {
 				$auth = true;
