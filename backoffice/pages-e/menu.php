d21c2bcfd6cc29722fb1dff7fca479e462d88759
Carlos Santos <c42759@users.noreply.github.com>
2017-09-15 10:11:38 +0100

    Update menu.php

--- backoffice/pages-e/menu.php
+++ backoffice/pages-e/menu.php
@@ -2,6 +2,12 @@
 
 $menu_item_tpl = bo3::loade("menu/item.tpl");
 
+$menu_fa_icon_tpl = bo3::loade("menu/fa-icon.tpl");
+$menu_img_tpl = bo3::loade("menu/img.tpl");
+
+$menu_sub_item_tpl = bo3::loade("menu/sub-item.tpl");
+$menu_item_w_sub_items_tpl = bo3::loade("menu/item-w-sub-items.tpl");
+
 $menu = "";
 $not_installed_menu = "";
 $installed_modules = [];
@@ -13,20 +19,59 @@ $query = sprintf(
 	"sort ASC"
 );
 
-$source = $db->query($query);
+$source = $mysqli->query($query);
 
 while ($data = $source->fetch_object()) {
 	array_push($installed_modules, $data->folder);
 
 	$tmp_name = explode("-", $data->folder);
+	$code = json_decode($data->code);
+
+	// SUB ITEMS
+	if (isset($code->{"sub-items"}) && count((array)$code->{"sub-items"}) > 0) {
+		$submenu = "";
+
+		foreach ((array)$code->{"sub-items"} as $index => $obj) {
+			$submenu .= bo3::c2r(
+				[
+					"url" => $obj->url,
+					"name" => $index
+				],
+				$menu_sub_item_tpl
+			);
+		}
+	}
+
+	// ICON
+	if (isset($code->img) && !empty($code->img)) {
+		$icon = bo3::c2r(
+			[
+				'module-folder' => $data->folder,
+				'img' => $code->img
+			],
+			$menu_img_tpl
+		);
+	} else {
+		$icon = bo3::c2r(
+			[
+				'module-folder' => $data->folder,
+				'fa' => (isset($code->{"fa-icon"}) && !empty($code->{"fa-icon"})) ? $code->{"fa-icon"} : "fa-folder"
+			],
+			$menu_fa_icon_tpl
+		);
+	}
 
 	$menu .= bo3::c2r(
 		[
+			"sub-menu" => (isset($submenu)) ? $submenu : "",
 			"mod" => $tmp_name[count($tmp_name) - 1],
-			"name" => $data->name
+			"name" => $data->name,
+			"icon" => $icon
 		],
-		$menu_item_tpl
+		(!isset($submenu)) ? $menu_item_tpl : $menu_item_w_sub_items_tpl
 	);
+
+	unset($submenu);
 }
 
 // show not installed modules if user has owner tag
