1ee8b2ddeea9c93686c553a3397b8f83ba441151
Rafael Duarte <rafaeljfduarte@gmail.com>
2018-08-21 13:05:33 +0100

    SQL updated to add log system and custom user field

--- install/base.sql
+++ install/base.sql
@@ -73,6 +73,14 @@ CREATE TABLE `os_trash` (
 	`date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+CREATE TABLE `os_log` (
+	`id` int(11) NOT NULL,
+	`user_id` int(11) NOT NULL,
+	`ip` text CHARACTER SET utf8 NOT NULL,
+	`code` text CHARACTER SET utf8 NOT NULL,
+	`date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 CREATE TABLE `os_users` (
 	`id` int(11) NOT NULL,
 	`username` varchar(255) DEFAULT NULL,
@@ -85,6 +93,18 @@ CREATE TABLE `os_users` (
 	`status` tinyint(1) NOT NULL DEFAULT '0',
 	`date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`date_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `os_users_fields` (
+	`id` int(11) NOT NULL,
+	`name` text CHARACTER SET utf8 NOT NULL,
+	`value` text CHARACTER SET utf8 NOT NULL,
+	`type` text CHARACTER SET utf8 NOT NULL,
+	`required` tinyint(1) NOT NULL DEFAULT '0',
+	`sort` int(11) NOT NULL,
+	`status` tinyint(1) NOT NULL DEFAULT '0',
+	`date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`date_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `os_articles`
@@ -112,12 +132,18 @@ ALTER TABLE `os_modules`
 ALTER TABLE `os_trash`
 	ADD PRIMARY KEY (`id`);
 
+ALTER TABLE `os_log`
+	ADD PRIMARY KEY (`id`);
+
 ALTER TABLE `os_users`
 	ADD PRIMARY KEY (`id`),
 	ADD UNIQUE KEY `name` (`username`),
 	ADD UNIQUE KEY `email` (`email`),
 	ADD KEY `fk_prefix_users_prefix_products1` (`id`),
 	ADD KEY `fk_prefix_users_prefix_articles1` (`id`);
+
+ALTER TABLE `os_users_fields`
+	ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `os_articles`
 	MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
@@ -146,10 +172,10 @@ ALTER TABLE `os_trash`
 ALTER TABLE `os_users`
 	MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-INSERT INTO `os_modules` (`name`, `folder`, `code`, `sort`) VALUES
-('Home', 'mod-5-home', '{\r\n	\"fa-icon\": \"fa-home\",\r\n	\"img\": \"\",\r\n	\"sub-items\": {}\r\n}', 0),
-('Account', 'mod-6-account', '{\r\n	\"fa-icon\": \"fa-user\",\r\n	\"img\": \"\",\r\n	\"sub-items\": {}\r\n}', 1),
-('Users', 'mod-9-users', '{\r\n	\"fa-icon\": \"fa-users\",\r\n	\"img\": \"\",\r\n	\"sub-items\": {\r\n		\"List\": {\r\n			\"url\": \"\"\r\n		},\r\n		\"Add user\": {\r\n			\"url\": \"add\"\r\n		}\r\n	}\r\n}', 2),
-('Categories', 'mod-8-categories', '{\r\n	\"fa-icon\": \"fa-list\",\r\n	\"img\": \"\",\r\n	\"sub-items\": {\r\n		\"List\": {\r\n			\"url\": \"\"\r\n		},\r\n		\"Add category\": {\r\n			\"url\": \"add\"\r\n		}\r\n	}\r\n}', 3),
-('Articles', 'mod-7-articles', '{\r\n	\"fa-icon\": \"fa-newspaper\",\r\n	\"img\": \"\",\r\n	\"sub-items\": {\r\n		\"List\": {\r\n			\"url\": \"\"\r\n		},\r\n		\"Add Article\": {\r\n			\"url\": \"add\"\r\n		}\r\n	}\r\n}', 4),
-('Files', 'mod-4-files', '{\r\n	\"fa-icon\": \"fa-file\",\r\n	\"img\": \"\",\r\n	\"sub-items\": {}\r\n}', 5);
+INSERT INTO `os_modules` (`id`, `name`, `folder`, `code`, `sort`) VALUES
+(1, 'Home', 'mod-5-home', '{\r\n	\"fa-icon\": \"fa-home\",\r\n	\"img\": \"\",\r\n	\"sub-items\": {},\r\n\"sidebar\": true,\r\n\"dropdown\": false\r\n}', 0),
+(2, 'Account', 'mod-6-account', '{\r\n	\"fa-icon\": \"fa-user-cog\",\r\n	\"img\": \"\",\r\n	\"sub-items\": {},\r\n\"sidebar\": false,\r\n\"dropdown\": true\r\n}', 1),
+(3, 'Users', 'mod-9-users', '{\r\n	\"fa-icon\": \"fa-users\",\r\n	\"img\": \"\",\r\n	\"sub-items\": {\r\n		\"List\": {\r\n			\"url\": \"\"\r\n		},\r\n		\"Add user\": {\r\n			\"url\": \"add\"\r\n		},\r\n\"Logs\": {\r\n			\"url\": \"logs\"\r\n		}\r\n	},\r\n\"sidebar\": true,\r\n\"dropdown\": false\r\n}', 2),
+(4, 'Categories', 'mod-8-categories', '{\r\n	\"fa-icon\": \"fa-list\",\r\n	\"img\": \"\",\r\n	\"sub-items\": {\r\n		\"List\": {\r\n			\"url\": \"\"\r\n		},\r\n		\"Add category\": {\r\n			\"url\": \"add\"\r\n		}\r\n	},\r\n\"sidebar\": true,\r\n\"dropdown\": false\r\n}', 3),
+(5, 'Articles', 'mod-7-articles', '{\r\n	\"fa-icon\": \"fa-newspaper\",\r\n	\"img\": \"\",\r\n	\"sub-items\": {\r\n		\"List\": {\r\n			\"url\": \"\"\r\n		},\r\n		\"Add Article\": {\r\n			\"url\": \"add\"\r\n		}\r\n	},\r\n\"sidebar\": true,\r\n\"dropdown\": false\r\n}', 4),
+(6, 'Files', 'mod-4-files', '{\r\n	\"fa-icon\": \"fa-file\",\r\n	\"img\": \"\",\r\n	\"sub-items\": {},\r\n\"sidebar\": true,\r\n\"dropdown\": false\r\n}', 5);
