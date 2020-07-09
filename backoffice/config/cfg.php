0c59060ba006198df51967a29711cc07babebb55
Carlos Santos <carlos@one-shift.com>
2020-07-09 15:50:29 +0100

    Fix path for FO Configs

--- backoffice/config/cfg.php
+++ backoffice/config/cfg.php
@@ -1,8 +1,15 @@
 <?php
 
+
+if (is_dir("./backoffice")) {
+	$path_to_backoffice = "/backoffice";
+} else {
+	$path_to_backoffice = "";
+}
+
 $cfg = new stdClass();
 
-include "config/database.php";
-include "config/email.php";
-include "config/languages.php";
-include "config/system.php";
+include ROOT_DIR."{$path_to_backoffice}/config/database.php";
+include ROOT_DIR."{$path_to_backoffice}/config/email.php";
+include ROOT_DIR."{$path_to_backoffice}/config/languages.php";
+include ROOT_DIR."{$path_to_backoffice}/config/system.php";
