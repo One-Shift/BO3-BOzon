7942dc1ca25d474f1cbe09c75c0b1253d178855a
Carlos Santos <carlos@one-shift.com>
2021-03-05 17:57:14 +0000

    fix custom_css default value

--- install/add-demo-user.sql
+++ install/add-demo-user.sql
@@ -1,1 +1,1 @@
-INSERT INTO `os_9_users` (`username`, `password`, `rank`, `email`, `user_key`, `status`) VALUES ('Demo', '9ccc4065e071a93e89b4327bb48b2aefe4f51a3e', 'owner', 'demo@demo.demo', '1794560797a5a5cc40364149fd623df8', 1);
+INSERT INTO `os_9_users` (`username`, `password`, `rank`, `email`, `user_key`, `status`, `custom_css`) VALUES ('Demo', '9ccc4065e071a93e89b4327bb48b2aefe4f51a3e', 'owner', 'demo@demo.demo', '1794560797a5a5cc40364149fd623df8', 1, '');
